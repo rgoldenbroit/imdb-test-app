fe561b2384c07bc70332f938c0a070c8981ec858
Russell Goldenbroit <rgoldenbroit@google.com>
2019-02-16 13:45:46 -0500

    Created first app

--- app.py
+++ app.py
@@ -2,30 +2,62 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
+
+import plotly.plotly as py
+from plotly.graph_objs import *
 
 ########### Set up the chart
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Summer Ale']
 
-bitterness = go.Bar(
-    x=beers,
-    y=[35, 60, 85, 20],
-    name='IBU',
-    marker={'color':'lightblue'}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=[5.4, 7.1, 9.2, 3],
-    name='ABV',
-    marker={'color':'pink'}
-)
+#Reads in static data file.
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = 'Beer Comparison'
-)
+source = '/Users/rgoldenbroit/Documents/GeneralAssembly/Final/imdb-test-app//imdb_1000.csv'
+df = pd.read_csv(source)
 
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
+#Define bar chat variables and display bar chart.
+
+newvar = df.groupby('content_rating')['duration'].mean().sort_values().tolist()
+newvar2 = df.groupby('content_rating')['duration'].mean().sort_values().keys().tolist()
+
+fig = go.Bar(x=newvar2, y=newvar,
+        name="Move duration vs content rating",
+        marker = {'color:lightblue'},
+        text=newvar
+            )
+
+graph_layout = go.Layout(
+    title = 'Are more mature movies generally longer?'')
+
+newfigure = go.Figure(layout=beer_layout)
+
+
+# beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Summer Ale']
+#
+# bitterness = go.Bar(
+#     x=beers,
+#     y=[35, 60, 85, 20],
+#     name='IBU',
+#     marker={'color':'lightblue'}
+# )
+# alcohol = go.Bar(
+#     x=beers,
+#     y=[5.4, 7.1, 9.2, 3],
+#     name='ABV',
+#     marker={'color':'pink'}
+# )
+#
+# beer_data = [bitterness, alcohol]
+# beer_layout = go.Layout(
+#     barmode='group',
+#     title = 'Beer Comparison'
+# )
+#
+# beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
 ########### Display the chart
 
@@ -33,10 +65,10 @@ app = dash.Dash()
 server = app.server
 
 app.layout = html.Div(children=[
-    html.H1('Charlie & The Chocolate Factory'),
+    html.H1('IMDB-based Cool First App'),
     dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
+        id='imdb1',
+        figure=newfigure
     )]
 )
 
